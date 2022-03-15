b27a1f6528775e045cf930cc4d546c6830a8629e
Krishna Sudarshan J <j.krishna.sudarshan2002@gmail.com>
2022-03-16 00:35:08 +0530

    Validation of Registration and Login Done

--- app/src/main/java/com/example/unihire/ApplicantRegistration.java
+++ app/src/main/java/com/example/unihire/ApplicantRegistration.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ApplicantRegistration extends AppCompatActivity {
     EditText name,email,pw,pw2;
@@ -42,10 +46,50 @@ public class ApplicantRegistration extends AppCompatActivity {
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RegisterApplicant();
+                if(
+                    name.getText().toString()!="" || email.getText().toString()!="" || pw.getText().toString()!="" ||
+                    pw2.getText().toString()!=""
+                ){
+                    if(noNumber(name.getText().toString())){
+                        if(Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+                            if(isValidPassword(pw.getText().toString())){
+                                if(pw2.getText().toString().equals(pw.getText().toString())){
+                                    RegisterApplicant();
+                                }else
+                                    Toast.makeText(ApplicantRegistration.this, "Confirm Password is not same", Toast.LENGTH_SHORT).show();
+                            }else
+                                Toast.makeText(ApplicantRegistration.this, "Password must contain 8 letters(One capital letter), a number and a special character", Toast.LENGTH_SHORT).show();
+
+                        }else
+                            Toast.makeText(ApplicantRegistration.this, "Enter proper email", Toast.LENGTH_SHORT).show();
+                    }else
+                        Toast.makeText(ApplicantRegistration.this, "Applicant name should not contain numbers", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(ApplicantRegistration.this, "Please fill empty inputs", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+    }
+    public boolean noNumber(String s){
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char c : chars){
+            if(Character.isDigit(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isValidPassword(String password)
+    {
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+        return m.matches();
     }
     public void RegisterApplicant(){
         progressBar.setVisibility(View.VISIBLE);
