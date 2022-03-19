17a947263f2a42e6ba5d4cf5a35df2dd1b852c75
Vishal Karthikeyan <vishalkarthikeyan17@gmail.com>
2022-03-19 23:04:49 +0530

    db insert for draft and Form Validation for post job done + UI Fixes

--- app/src/main/java/com/example/unihire/MainActivity.java
+++ app/src/main/java/com/example/unihire/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.unihire;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.graphics.Paint;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         Button goToLoginRec=findViewById(R.id.goToLoginBtnRec);
         Button goToLoginApp=findViewById(R.id.goToLoginBtnApp);
         TextView goToRegApp=findViewById(R.id.goToRegApp);
