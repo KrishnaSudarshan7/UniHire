dfe88c2e3edc45a44adbda0d60c8874f34315e21
Vishal Karthikeyan <vishalkarthikeyan17@gmail.com>
2022-03-24 20:31:02 +0530

    Applicant Edit Page

--- app/src/main/java/com/example/unihire/uiRec/ProfileRec/ProfileFragment.java
+++ app/src/main/java/com/example/unihire/uiRec/ProfileRec/ProfileFragment.java
@@ -1,20 +1,23 @@
 package com.example.unihire.uiRec.ProfileRec;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.unihire.R;
+import com.example.unihire.Applicant_Edit_Profile;
 
 public class ProfileFragment extends Fragment {
 
 
-
+    Button edit;
     TextView text;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
